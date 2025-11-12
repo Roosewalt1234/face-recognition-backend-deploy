588ce5a9e996969f6bb49159803dd622a4a52e84
Roosewalt1234 <roosewalt@gmail.com>
2025-11-12 16:45:05 +0400

    Add CORS middleware for frontend requests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from datetime import datetime, timezone
 from typing import Optional, List, Dict
